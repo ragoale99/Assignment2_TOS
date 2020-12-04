ef3ffbcbc2ba639a90ac82489a594670600304dd
Raghesh99 <raghesh99@gmail.com>
2020-12-04 12:26:02 +0100

    Configurazione

---
+++ src/main/java/it/unipd/tos/App.java
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Alessandro Rago 1187504
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
